61461ebbc0f9b5665c1f6e3bbecdb9167de8ec63
muridsifo23santuy <fawwazdh05@gmail.com>
2023-12-12 09:18:11 +0700

    update

--- src/quiz/bp/QUIZBP.java
+++ src/quiz/bp/QUIZBP.java
@@ -9,34 +9,50 @@ package quiz.bp;
  * @author LENOVO
  */
 import java.util.Scanner;
+
 public class QUIZBP {
 
     public static void main(String[] args) {
-        int angka[]= {3300,1022,7800,3400,6000};
-        int i,j;
-        
-        for (i = 0; i < angka.length;i++){
+        int angka[] = {3300, 1022, 7800, 3400, 6000};
+        int i, j;
+
+        for (i = 0; i < angka.length; i++) {
             System.out.println(angka[i]);
         }
-        System.out.println("sorting from as : ");
-         for (i = 0; i < angka.length;i++){
-             for(j = i;j<angka.length-1;j++){
-                 if(angka[j] > angka[j+1]){
-                     int temp = angka[j];
-                     angka [j]= angka[j+1];
-                     angka [j+1]= temp;
-                     
-                 }
-                 
-             }
-             
-         }
-           for (i = 0; i < angka.length;i++){
+        System.out.println("sorting dari terkecil : ");
+        for (i = 0; i < angka.length; i++) {
+            for (j = i; j < angka.length - 1; j++) {
+                if (angka[j] > angka[j + 1]) {
+                    int temp = angka[j];
+                    angka[j] = angka[j + 1];
+                    angka[j + 1] = temp;
+
+                }
+
+            }
+
+        }
+        for (i = 0; i < angka.length; i++) {
             System.out.println(angka[i]);
-           }
-         
-                
-       
+        }
+
+        System.out.println("sorting dari terbesar : ");
+        for (i = 0; i < angka.length; i++) {
+            for (j = 0; j < angka.length - 1; j++) {
+                if (angka[j] < angka[j + 1]) {
+                    int temp = angka[j];
+                    angka[j] = angka[j + 1];
+                    angka[j + 1] = temp;
+
+                }
+
+            }
+
+        }
+        for (i = 0; i < angka.length; i++) {
+            System.out.println(angka[i]);
+        }
+
     }
-    
+
 }
